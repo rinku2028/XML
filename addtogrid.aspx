341567da78b52abb5a3f5cc7ad51f7078d55e202
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 14:15:53 +0545

    Adding xml file data in gridview

---
+++ addtogrid.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="addtogrid.aspx.cs" Inherits="xmlapp.addtogrid" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnclick" runat="server" Text="Click" OnClick="btnclick_Click" /> </br>
+            <asp:GridView ID="gdview1" runat="server"></asp:GridView>
+            <asp:Label ID="lblmessage" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ addtogrid.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace xmlapp
+{
+    public partial class addtogrid : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnclick_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XmlReader xmlFile;
+                xmlFile = XmlReader.Create("C:/Users/Dell/source/repos/xmlapp/Product.xml", new XmlReaderSettings());
+                DataSet ds = new DataSet();
+                ds.ReadXml(xmlFile);
+                gdview1.DataSource = ds.Tables[0];
+                gdview1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblmessage.Text = ex.ToString();
+            }
+        }
+    }
+}
