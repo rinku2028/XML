7ddadb1e59a34050abdb63271ad869171067a76c
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 13:45:21 +0545

    Writes xml file using c#

---
+++ xmlfilewrite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace xml
+{
+    class xmlfilewrite
+    {
+        static void Main(string []args)
+        {
+            // Create an isntance of XmlTextReader and call Read method to read the file  
+            XmlWriter xmlWriter = XmlWriter.Create("C:/Users/Dell/source/repos/xml/xmlfilewriter.xml");
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("Users");
+
+            xmlWriter.WriteStartElement("User");
+            xmlWriter.WriteAttributeString("Age", "22");
+            xmlWriter.WriteString("John Doe");
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("User");
+            xmlWriter.WriteAttributeString("Age", "33");
+            xmlWriter.WriteString("Jane Doe");
+
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+            Console.ReadLine();
+        }
+        }
+    }
+
