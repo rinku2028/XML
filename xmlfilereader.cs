1e98196e97f73a438817bb02d16515c495283fcb
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 13:27:23 +0545

    Read data from info.xml file

---
+++ xmlfilereader.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Data;
+using System.Xml;
+
+namespace xml
+{
+    class helloworld
+    {
+        static void Main(string[] args)
+        {
+            
+            // Create XmlReader object  and setup Stream between XML file and XML Reader
+            using (XmlReader reader = XmlReader.Create(@"C:/Users/Dell/source/repos/xml/info.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        //return only when you have START tag  
+                        switch (reader.Name.ToString())
+                        {
+                            case "Name":
+                                Console.WriteLine("Name of the Element is : " + reader.ReadString());
+                                break;
+                            case "Location":
+                                Console.WriteLine("Your Location is : " + reader.ReadString());
+                                break;
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            Console.ReadKey();
+        }
+        }
+    
+}
